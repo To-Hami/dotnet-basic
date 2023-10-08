1dbd54dfa2450aaa9b660d6b7e7934ce9b7c2098
Tohami <tohami00076@gmail.com>
2023-10-08 08:14:59 -0700

    first

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace myApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // create your app using dotnet new console 
+            // use system for short cut command 
+            // name your namspace 
+            // name your class 
+            // name your function 
+            // handel your function 
+            // run your app using dotnet run 
+            Console.WriteLine("hello tohami");
+        }
+    }
+
+}
